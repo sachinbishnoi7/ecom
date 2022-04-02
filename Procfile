78a39b995483d919cf2d984f05d1dc01f9ac557c
Sachin Bishnoi <sachinkhicher363@gmail.com>
2022-04-02 10:57:48 +0530

    all files

---
+++ Procfile
@@ -0,0 +1,5 @@
+web: gunicorn ecom.wsgi
+release: python manage.py makemigrations --noinput
+release: python manage.py collectstatic --noinput
+release: python manage.py migrate --noinput
+
---
+++ api/product/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Product
+class ProdcuctSerializer(serializers.HyperlinkedModelSerializer):
+    image = serializers.ImageField(max_length=None, allow_empty_file= False, allow_null = True, required= False ) #new 
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'description', 'price', 'image', 'category')
+   
